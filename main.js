895abe82b457f9f8f5c510d1d2223ea23fa6fd0c
Dubem <dubykol@gmail.com>
2025-09-09 14:02:03 +0100

    feat: add playRound and playGame function

--- main.js
+++ main.js
@@ -1,25 +1,24 @@
-let humanScore = 0;
-let computerScore = 0;
-
 let computerChoice = Math.random()
 const userInput =  prompt("What's your choice?")
+let computerMove = ""
 
 function getComputerChoice(){
     if(computerChoice <= 0.3 ){
-        computerChoice = "rocks"
+        computerMove = "rocks"
     }
     else if(computerChoice > 0.3 && computerChoice <= 0.6){
-        computerChoice = "paper"
+        computerMove = "paper"
     }
     else if(computerChoice > 0.6 && computerChoice <= 0.9)
-       { computerChoice = "scissors" }
-    console.log(computerChoice) 
+       { computerMove = "scissors" }
+    console.log(computerMove) 
+    return(computerMove)
 }
 getComputerChoice();
 
 function getHumanChoice(){
    let humanChoice = userInput
-   console.log(humanChoice)
+  console.log(humanChoice)
     return humanChoice;
 } 
 
@@ -28,27 +27,34 @@ getHumanChoice();
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
-function playRound(humanChoice, computerChoice) {
-if (humanChoice === computerChoice){
-    console.log("This is a draw")
-}
-if(humanChoice === "Rocks" && computerChoice === "Paper"){
-    console.log("You win")
-        humanScore += 1
-}
+function playGame(){
+let humanScore = 0;
+let computerScore = 0;
 
-if(humanChoice === "Paper" && computerChoice === "Scissors"){
-    console.log("You win")
-        humanScore += 1
+function playRound(humanChoice, computerMove) {
+if (humanChoice === computerMove){
+   console.log("A draw")
 }
-
-if(humanChoice === "Scissors" && computerChoice === "Rocks"){
-    console.log("You win")
-        humanScore += 1
+else if(humanChoice === "rocks" && computerMove === "paper"){
+    console.log("The player wins. Rock beats Paper")
+}
+else if(humanChoice === "paper" && computerMove === "scissors"){
+ console.log("The player wins. Paper beats scissors")
+}
+else if(humanChoice === "scissors" && computerMove === "rocks"){
+ console.log("The player wins. scissors beats rocks")
+}
+else{
+    console.log("computer wins")
 }
 }
 
 
 playRound(humanSelection, computerSelection);
+
+
+
+}
+console.log(playGame())
 
 
